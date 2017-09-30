9883874f29f1994ae694e976a19a865d4e0235f2
Manoj km <tvmmanu@gmail.com>
2017-09-30 20:31:38 +0530

    s

--- grunt/autoprefixer.js
+++ grunt/autoprefixer.js
@@ -1,8 +1,13 @@
 module.exports = {
+
+    options: {
+        map: true //Autoprefixer will not remove sourcemap info from scss file.
+    },
+
     dev: {
         expand: true,
         flatten: true,
-        src: '<%= cssCombPath %>',
+        src: '<%= cssCombPath %>', // path is configured in Gruntfile.js
         dest: '<%= site.dev_assets %>/css/',
         nonull: true, //to warn if a given file is missing or invalid
     }
--- grunt/copy.js
+++ grunt/copy.js
@@ -2,31 +2,31 @@ module.exports = {
 
     vendor: {
         files: [
-            { expand: true, cwd: '<%= site.src %>/vendor/', src: ['**'], dest: '<%= site.dev %>/vendor/' },
+            {expand: true, cwd: '<%= site.src %>/vendor/', src: ['**'], dest: '<%= site.dev %>/vendor/'},
         ],
     },
 
     css: {
         files: [
-            { expand: true, cwd: '<%= site.src_assets %>/css/', src: ['**'], dest: '<%= site.dev_assets %>/css/' },
+            {expand: true, cwd: '<%= site.src_assets %>/css/', src: ['**'], dest: '<%= site.dev_assets %>/css/'},
         ],
     },
 
     js: {
         files: [
-            { expand: true, cwd: '<%= site.src_assets %>/js/', src: ['**'], dest: '<%= site.dev_assets %>/js/' },
+            {expand: true, cwd: '<%= site.src_assets %>/js/', src: ['**'], dest: '<%= site.dev_assets %>/js/'},
         ],
     },
 
     images: {
         files: [
-            { expand: true, cwd: '<%= site.src_assets %>/img/', src: ['**'], dest: '<%= site.dev_assets %>/img/' },
+            {expand: true, cwd: '<%= site.src_assets %>/img/', src: ['**'], dest: '<%= site.dev_assets %>/img/'},
         ],
     },
 
     fonts: {
         files: [
-            { expand: true, cwd: '<%= site.src_assets %>/fonts/', src: ['**'], dest: '<%= site.dev_assets %>/fonts/' },
+            {expand: true, cwd: '<%= site.src_assets %>/fonts/', src: ['**'], dest: '<%= site.dev_assets %>/fonts/'},
         ],
     },
 
