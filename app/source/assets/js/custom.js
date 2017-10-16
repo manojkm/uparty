54b65a2ecde3ac81fbff27489cf5d188ed948e43
Manoj km <tvmmanu@gmail.com>
2017-10-16 20:01:52 +0530

    s

--- app/source/assets/js/custom.js
+++ app/source/assets/js/custom.js
@@ -77,3 +77,18 @@ $(document).on("chl.plugin", function () {
     });
 
 }).trigger("chl.plugin");
+
+
+$(document).on("chl.dropdown", function () {
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+
+}).trigger("chl.dropdown");
