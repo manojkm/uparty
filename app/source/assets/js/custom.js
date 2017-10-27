417f2d9f6fd5a924df8371d7ab9a40deb47996c6
Manoj <tvmmanu@gmail.com>
2017-10-27 17:32:59 +0530

    sidebar footer

--- app/source/assets/js/custom.js
+++ app/source/assets/js/custom.js
@@ -3,12 +3,16 @@ if (typeof jQuery === 'undefined') {
     throw new Error('Theme\'s JavaScript requires jQuery');
 }
 
+
 var appMaster = {
 
     _body: $('body'),
     _logo: $('.sidebar__brand__logo'),
+    _side_nav: $('.sidebar__nav'),
     _aside: $("[data-aside='show']"),
     _mini: $("[data-side='mini']"),
+    _hide: $("[data-side='hide']"),
+    _overlay: $('.overlay'),
 
     responsive: function () {
         $(window).width() < 768 ? appMaster._body.removeClass('sidebar-mini sidebar-is-open').addClass('sidebar-is-closed') : appMaster._body.addClass('sidebar-is-open').removeClass('sidebar-is-closed');
@@ -17,7 +21,7 @@ var appMaster = {
 
     sidebar: function () {
 
-        $("[data-side='hide']").on('click', function (event) {
+        $(appMaster._hide).on('click', function (event) {
             event.preventDefault();
             $(this).toggleClass('collapsed');
             appMaster._body.removeClass('sidebar-mini aside-is-open').toggleClass('sidebar-is-open sidebar-is-closed');
@@ -26,7 +30,7 @@ var appMaster = {
             appMaster._stopMetisMenu();
         });
 
-        $("[data-side='mini']").on('click', function (event) {
+        $(appMaster._mini).on('click', function (event) {
             event.preventDefault();
             $(this).toggleClass('collapsed');
             appMaster._body.toggleClass('sidebar-mini').removeClass('aside-is-open');
@@ -38,29 +42,45 @@ var appMaster = {
     },
 
     aside: function () {
+
+        var asideisOpen = false;
+
         appMaster._aside.on('click', function (event) {
             event.preventDefault();
             $(this).toggleClass('collapsed');
-
-            if (appMaster._body.hasClass('sidebar-mini') || appMaster._body.hasClass('sidebar-is-closed')) {
-                appMaster._body.toggleClass('aside-is-open');
-            }
-
-            else if (!appMaster._body.hasClass('sidebar-mini') || !appMaster._body.hasClass('sidebar-is-open')) {
-                appMaster._body.toggleClass('aside-is-open sidebar-mini');
+            // Adapted from https://codepen.io/j_holtslander/pen/XmpMEp TODO, nice adaption, so pls learn and correct the above methods
+            if (asideisOpen == false) {
+                appMaster._body.addClass('aside-is-open sidebar-mini' );
                 appMaster._mini.addClass('collapsed');
+                appMaster._overlay.show();
+                asideisOpen = true;
+            }
+            else {
+                appMaster._body.removeClass('aside-is-open sidebar-mini');
+                appMaster._mini.removeClass('collapsed');
+                appMaster._overlay.hide();
+                asideisOpen = false;
             }
 
+            /*    $(this).toggleClass('collapsed');
+             if (appMaster._body.hasClass('sidebar-mini') || appMaster._body.hasClass('sidebar-is-closed')) {
+             appMaster._body.toggleClass('aside-is-open');
+             }
+
+             else if (!appMaster._body.hasClass('sidebar-mini') || !appMaster._body.hasClass('sidebar-is-open')) {
+             appMaster._body.toggleClass('aside-is-open sidebar-mini');
+             appMaster._mini.addClass('collapsed');
+             }*/
             appMaster._stopMetisMenu();
         });
     },
 
 
     _stopMetisMenu: function () {
-        $('.sidebar__nav').find('li').removeClass('active');
-        $('.sidebar__nav').find('a').attr('aria-expanded', false);
+        $(appMaster._side_nav).find('li').removeClass('active');
+        $(appMaster._side_nav).find('a').attr('aria-expanded', false);
         // $('.sidebar__nav').find('a').removeClass('has-arrow');
-        $('.sidebar__nav').find('ul.collapse').removeClass('in').attr('aria-expanded', false);
+        $(appMaster._side_nav).find('ul.collapse').removeClass('in').attr('aria-expanded', false);
     },
 
     _changeLogo: function () {
@@ -89,7 +109,7 @@ var Pluggin = {
         } else {
             throw new Error('First install metisMenu plugin! https://github.com/onokumus/metisMenu');
         }
-    },
+    }
 
 };
 
