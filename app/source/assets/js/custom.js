355eb27e1601ff07d3d241280621b3f58f6e469a
Manoj km <tvmmanu@gmail.com>
2018-01-25 01:14:12 +0530

    s

--- app/source/assets/js/custom.js
+++ app/source/assets/js/custom.js
@@ -289,10 +289,10 @@ var Pluggin = {
             var config = {
                 '.chosen-select'           : {width: '100%'},
                 '.chosen-select-deselect'  : { allow_single_deselect: true, width: '100%' },
-                '.chosen-select-no-single' : { disable_search_threshold: 10 },
-                '.chosen-select-no-results': { no_results_text: 'Oops, nothing found!' },
+                '.chosen-select-no-single' : { disable_search_threshold: 10, width: '100%'  },
+                '.chosen-select-no-results': { no_results_text: 'Oops, nothing found!', width: '100%' },
                 '.chosen-select-rtl'       : { rtl: true },
-                '.chosen-select-width'     : { width: '95%' }
+                '.chosen-select-width'     : { width: '100%' }
             };
             for (var selector in config) {
                 $(selector).chosen(config[selector]);
