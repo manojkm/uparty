a4d33adff1beb09f27a8c30ef157b4873d535cdb
Manoj <tvmmanu@gmail.com>
2018-02-22 17:58:32 +0530

    spinner

--- app/source/assets/js/custom.js
+++ app/source/assets/js/custom.js
@@ -431,7 +431,7 @@ var appMaster = {
             var input = btn.closest('.number-spinner').find('input');
             btn.closest('.number-spinner').find('button').prop("disabled", false);
 
-            if (btn.attr('data-dir') == 'up') {
+            if (btn.attr('data-action') == 'increment') {
                 action = setInterval(function(){
                     if ( input.attr('max') == undefined || parseInt(input.val()) < parseInt(input.attr('max')) ) {
                         input.val(parseInt(input.val())+1);
@@ -440,7 +440,7 @@ var appMaster = {
                         clearInterval(action);
                     }
                 }, 50);
-            } else {
+            } else  if (btn.attr('data-action') == 'decrement') {
                 action = setInterval(function(){
                     if ( input.attr('min') == undefined || parseInt(input.val()) > parseInt(input.attr('min')) ) {
                         input.val(parseInt(input.val())-1);
