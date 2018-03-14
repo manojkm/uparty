2c63e2b21f543c5808d83fce45cc84d9b93409db
Manoj <tvmmanu@gmail.com>
2018-03-14 18:14:21 +0530

    form tooltip

--- app/source/assets/js/custom.js
+++ app/source/assets/js/custom.js
@@ -1107,7 +1107,7 @@ var appMaster = {
             return this.optional(element) || /\d{5}-\d{4}$|^\d{5}$/.test(value)
         }, "The specified US ZIP Code is invalid");
 
-        // validate signup form on keyup and submit
+        //Advanced validation
         $("#signupForm").validate({
 
             validClass: 'is-valid',
@@ -1181,9 +1181,97 @@ var appMaster = {
             }
         });
 
+        //Advanced validation with tooltip
+        $("#signupFormTooltip").validate({
+
+            showErrors: function(errorMap, errorList) {
+
+                // Clean up any tooltips for valid elements
+                $.each(this.validElements(), function (index, element) {
+                    var $element = $(element);
+
+                    $element.data("title", "") // Clear the title - there is no error associated anymore
+                        .removeClass("is-invalid")
+                        .tooltip("dispose");
+                });
+
+                // Create new tooltips for invalid elements
+                $.each(errorList, function (index, error) {
+                    var $element = $(error.element);
+
+                    $element.tooltip("dispose") // Destroy any pre-existing tooltip so we can repopulate with new tooltip content
+                        .data("title", error.message)
+                        .addClass("is-invalid")
+                        .tooltip({
+                            // Position of the tooltip on top, bottom, left or the right side of the element:
+                            placement:"top",
+                            template:'<div class="tooltip tooltip-danger" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
+
+                        }); // Create a new tooltip based on the error messsage we just set in the title
+                });
+            },
+
+            highlight: function ( element, errorClass, validClass ) {
+                // $( element ).addClass( errorClass ).removeClass(validClass);
+                $( element ).addClass( errorClass );
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                // $( element ).addClass(validClass).removeClass(errorClass);
+                $( element ).removeClass(errorClass);
+            },
+
+            rules: {
+                username: {
+                    minlength:4
+                },
+                password: {
+                    minlength: 6
+                },
+                confirm_password: {
+                    minlength: 6,
+                    equalTo: "#password-5"
+                },
+                email: {
+                    email: true
+                },
+                zip: {
+                    zipcodeUS: true
+                },
+                agree: "required"
+            },
+            messages: {
+                firstname: "Please enter your firstname",
+                lastname: "Please enter your lastname",
+                username: {
+                    required: "Please enter a username",
+                    minlength: "Your username must consist of at least 4 characters"
+                },
+                password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 6 characters long"
+                },
+                confirm_password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 6 characters long",
+                    equalTo: "Please enter the same password as above"
+                },
+                zip: {
+                    required: "Please enter your zip code!"
+                },
+                city: "Please enter your city",
+                state: "Please select your state",
+                email: "Please enter a valid email address",
+                agree: "Please accept our policy"
+            },
+
+            submitHandler: function(form) {
+                alert("This is a valid form!");
+            }
+        });
+
         // On clicking reset button, every error should be cleared
-        $('#clearform').on('click', function () {
-            $("#signupForm").validate().resetForm();  // clear out the validation errors
+        $('.clearform').on('click', function () {
+            $("#signupForm", "#signupFormTooltip").validate().resetForm();  // clear out the validation errors
         });
 
     },
