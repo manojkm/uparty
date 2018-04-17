36460195774a43ea414e006ffe7ddd4c03f28d71
Manoj km <tvmmanu@gmail.com>
2018-04-18 00:36:04 +0530

    s

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,32 +1,42 @@
 module.exports = {
-    dev: {
-        // Enble the below if you are using grunt-sass
-            /* options: {
-            lineNumbers: true,
-            includePaths:[
+
+    // Enble the below if you are using grunt-sass
+    /* options: {
+     lineNumbers: true,
+     includePaths:[
+     require('node-bourbon').includePaths,
+     '<%= site.node_dir %>/bootstrap/scss',
+     ],
+     sourceMap: true,
+     outputStyle: 'expanded' //Values: nested, expanded, compact, compressed
+
+     },
+     */
+
+    // Enble the below if you are using grunt-contrib-sass
+    options: {
+        // loadPath: require('node-bourbon').includePaths,
+        loadPath:[
             require('node-bourbon').includePaths,
-            '<%= site.node_dir %>/bootstrap/scss',
-            ],
-            sourceMap: true,
-            outputStyle: 'expanded' //Values: nested, expanded, compact, compressed
+            '<%= site.node_dir %>/bootstrap/scss'
+        ],
 
-            },
-            */
+        style: 'expanded'
+    },
 
-        // Enble the below if you are using grunt-contrib-sass
-        options: {
-            // loadPath: require('node-bourbon').includePaths,
-            loadPath:[
-                require('node-bourbon').includePaths,
-                '<%= site.node_dir %>/bootstrap/scss',
-            ],
-
-            style: 'expanded',
-        },
-
+    dev: {
         files: {
-            '<%= site.dev_assets %>/css/style.css': '<%= site.src_assets %>/scss/style.scss',
-            // '<%= site.dev_assets %>/css/bootstrap.css': '<%= site.src_assets %>/scss/bootstrap.scss',
+            '<%= site.dev_assets %>/css/style.css': '<%= site.src_assets %>/scss/style.scss'
         }
+    },
+
+    dev2: {
+        files: [{
+            expand: true,
+            src: ['<%= site.src_assets %>/scss/vendors-extensions/*.scss'],
+            dest: '<%= site.dev_assets %>/css/vendors/',
+            ext: '.css'
+        }]
     }
+
 };
