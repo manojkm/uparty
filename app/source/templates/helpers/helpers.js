0f7f9028da85df8dc7d9cce03a48c8effd848df2
Manoj km <tvmmanu@gmail.com>
2018-06-07 01:35:17 +0530

    handlebars

--- app/source/templates/helpers/helpers.js
+++ app/source/templates/helpers/helpers.js
@@ -8,5 +8,22 @@ module.exports.register = function (Handlebars, options) {
             return '';
         }
     });
+    Handlebars.registerHelper('list', function(context, options) {
+        var ret = "<ul>";
+
+        for(var i=0, j=context.length; i<j; i++) {
+            ret = ret + "<li>" + options.fn(context[i]) + "</li>";
+        }
+
+        return ret + "</ul>";
+    });
+
+    Handlebars.registerHelper("greeting", function() {
+        return new Handlebars.SafeString("Hello World");
+    });
+
+    Handlebars.registerHelper("button", function(type, copy) {
+        return new Handlebars.SafeString('<button class="' + type + '">' + copy + '</button>');
+    });
 
 };
