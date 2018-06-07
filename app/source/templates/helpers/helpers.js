4ccd5f17dd48875ce439a0415bf58ebfb84f61c7
Manoj <tvmmanu@gmail.com>
2018-06-07 14:31:27 +0530

    s

--- app/source/templates/helpers/helpers.js
+++ app/source/templates/helpers/helpers.js
@@ -8,22 +8,36 @@ module.exports.register = function (Handlebars, options) {
             return '';
         }
     });
-    Handlebars.registerHelper('list', function(context, options) {
-        var ret = "<ul>";
 
-        for(var i=0, j=context.length; i<j; i++) {
+    // Usage:
+    // {#list data.nav}}<a href="{{url}}">{{title}}</a>{{/list}}
+    Handlebars.registerHelper('list', function (context, options) {
+        var ret = "<ul>";
+        for (var i = 0, j = context.length; i < j; i++) {
             ret = ret + "<li>" + options.fn(context[i]) + "</li>";
         }
-
         return ret + "</ul>";
     });
 
-    Handlebars.registerHelper("greeting", function() {
+
+    // Usage: {{ greeting }}
+    Handlebars.registerHelper("greeting", function () {
         return new Handlebars.SafeString("Hello World");
     });
 
-    Handlebars.registerHelper("button", function(type, copy) {
-        return new Handlebars.SafeString('<button class="' + type + '">' + copy + '</button>');
+    // Usage: {{#btn "primary"}}Primary{{/btn}}
+    Handlebars.registerHelper('btn', function (name, options) {
+        return '<button type="button" class="btn btn-' + name + '">' + options.fn(this) + '</button>';
     });
+
+    // Usage: {{#btn_list data.btn}} <button type="button" class="{{class}} btn-raised">{{text}}</button>{{/btn_list}}
+    Handlebars.registerHelper('btn_list', function (context, options) {
+        var ret = "";
+        for (var i = 0, j = context.length; i < j; i++) {
+            ret = ret + options.fn(context[i]);
+        }
+        return ret;
+    });
+
 
 };
