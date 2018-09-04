dd63ae2bd5821f76134f37f5f8b3c7dbbd012a44
Manoj km <tvmmanu@gmail.com>
2018-09-05 01:18:29 +0530

    selects

--- app/source/assets/js/vendors-extended/bootstrap-multiselect.js
+++ app/source/assets/js/vendors-extended/bootstrap-multiselect.js
@@ -21,4 +21,36 @@ $(document).ready(function () {
         }
     );
 
+
+    $('#bs-multiselect-4').multiselect({
+        enableClickableOptGroups: true,
+        enableFiltering: true,
+        includeSelectAllOption: true,
+       // buttonWidth: '100%',
+        maxHeight: 400,
+        dropUp: true,
+        buttonClass: 'btn btn-outline-primary',
+        templates: {
+            ul: '<ul class="multiselect-container dropdown-menu extended"></ul>',
+            filter: '<li class="multiselect-item filter"><div class="input-group input-group-sm"><div class="input-group-prepend"><span class="input-group-text"><i class="icon-magnifier"></i></span></div><input class="form-control multiselect-search" type="text"></div></li>',
+            filterClearBtn: '<div class="input-group-append"><button class="btn btn-light multiselect-clear-filter" type="button"><i class="icon-close"></i></button></div>',
+        }
+    });
+
+    $('#bs-multiselect-5').multiselect({
+        enableClickableOptGroups: true,
+        enableCollapsibleOptGroups: true,
+        collapseOptGroupsByDefault: true,
+        enableFiltering: true,
+        // buttonWidth: '100%',
+        maxHeight: 400,
+        dropUp: true,
+        buttonClass: 'btn btn-outline-primary',
+        templates: {
+            ul: '<ul class="multiselect-container dropdown-menu extended"></ul>',
+            filter: '<li class="multiselect-item filter"><div class="input-group input-group-sm"><div class="input-group-prepend"><span class="input-group-text"><i class="icon-magnifier"></i></span></div><input class="form-control multiselect-search" type="text"></div></li>',
+            filterClearBtn: '<div class="input-group-append"><button class="btn btn-light multiselect-clear-filter" type="button"><i class="icon-close"></i></button></div>',
+        }
+    });
+
 });
